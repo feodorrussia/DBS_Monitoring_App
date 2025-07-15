a3148227b365b1c5bfeaabe984816f99055d8519
Fyodor <34249952+feodorrussia@users.noreply.github.com>
2025-07-15 16:43:12 +0300

    Write function to calc magnitude
    !!Need to test!!

---
+++ sources/Data_processing.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+
+def complex_to_magnitude(df: pd.DataFrame, time: bool = True) -> pd.DataFrame:
+    """
+    Calculate magnitude from df of complex data
+    :param df: pd.DataFrame of the complex data
+    :param time: is first column is **t** - time
+    :return:
+    """
+    if time:
+        sq_data = df.drop(["t"]).power(2).to_numpy().T
+    else:
+        sq_data = df.power(2).to_numpy().T
+
+    magnitude = sq_data.reshape(-1, sq_data.shape[1], 2).sum(axis=2)
+
+    if time:
+        magnitude_df = pd.DataFrame(magnitude.T, columns=[f"ch{i}" for i in range(1, magnitude.shape[0] + 1)])
+        return pd.concat([df.t, magnitude_df])
+
+    return pd.DataFrame(magnitude.T, columns=[f"ch{i}" for i in range(1, magnitude.shape[0] + 1)])
