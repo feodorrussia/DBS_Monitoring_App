fdc3589d0883cbc5be9b955665115bc2a0d7dcff
Fyodor <34249952+feodorrussia@users.noreply.github.com>
2025-07-15 15:52:17 +0300

    Add function to load SHT
    Requires shtReader_py framework

---
+++ sources/Files_operating.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import os
+
+import shtReader_py.shtRipper as shtRipper
+
+
+def load_sht(file_path: str, column_names: list = None, data_names: list = None) -> pd.DataFrame:
+    """
+    Function to load data from .SHT files. Use shtRipper.ripper.read() from shtReader_py.
+    :param file_path: name of the file with **absolute** path
+    :param column_names: (optional) list of column names that need to be loaded
+    :param data_names: (optional) list of data names to rename loaded columns
+    :return: pd.DataFrame with all or selected (if columns_names defined) columns
+    """
+    if ".sht" in file_path.lower():
+        if not os.path.isfile(file_path):
+            raise FileNotFoundError
+        res = shtRipper.ripper.read(file_path)
+    else:
+        if not os.path.isfile(file_path + ".SHT"):
+            raise FileNotFoundError
+        res = shtRipper.ripper.read(file_path + ".SHT")
+
+    if column_names is None:
+        column_names = res.keys()
+
+    if data_names is None:
+        data_names = column_names
+    elif len(data_names) != len(column_names):
+        raise ValueError("Number of columns & names does not match")
+
+    data = np.array([res[column_names[0]]["x"]] + [res[column_name]["y"] for column_name in column_names])
+    dalpha_df = pd.DataFrame(data.transpose(), columns=["t"] + data_names)
+    return dalpha_df
